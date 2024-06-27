40a7a39b44c4b46ada5626bc3efbd6f30072252f
Turcan Ilya <ilieturcan424@gmail.com>
2024-06-27 21:39:09 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
 window.addEventListener("DOMContentLoaded", () => {
     function req() {
-
-        let body = {
-            name: "Someone",
-            surname: "Else",
-            age: 26,
-            id: Math.random()
-        };
-
-        let json = JSON.stringify(body); // Этот метод занимается тем, что берёт объект javascript и превращает его в формат json.
-
         const request = new XMLHttpRequest(); // Создали новый объект которые позволяет подгружать данные без перезагрузки страницы. Технология ajax.
-        request.open("POST", "http://localhost:3000/people"); // Данный метод позволяет указать тип запроса.
+        request.open("GET", "http://localhost:3000/people"); // Данный метод позволяет указать тип запроса.
         request.setRequestHeader("Content-type", "application/json; charset=utf-8");
-        request.send(json); // Отправляем из клиента запрос на сервер и отправляем наш json объект. 
+        request.send(); // Отправляем из клиента запрос на сервер. 
         // Как только мы отправили запрос на сервер, мы также получаем ответ от сервера. Дальше про это.
         request.addEventListener("readystatechange", () => {
             if (request.readyState === 4 && request.status == 200) {
@@ -26,29 +16,29 @@ window.addEventListener("DOMContentLoaded", () => {
 
                 // Дальше работаем с данными в зависимости от того, какие именно данные нам предоставил сервер
 
-                // data.forEach(item => {
-                //     let card = document.createElement("div");
+                data.forEach(item => {
+                    let card = document.createElement("div");
 
-                //     card.classList.add("card");
+                    card.classList.add("card");
 
-                //     let icon;
-                //     if (item.sex == "male") {
-                //         icon = "icons/mars.png";
-                //     } else {
-                //         icon = "icons/female.png";
-                //     }
+                    let icon;
+                    if (item.sex == "male") {
+                        icon = "icons/mars.png";
+                    } else {
+                        icon = "icons/female.png";
+                    }
 
-                //     card.innerHTML = `
-                //         <img src ="${item.photo}" alt="photo">
-                //         <div class"name">${item.name} ${item.surname}</div>
-                //         <div class="sex">
-                //             <img src=${icon} alt="male">
-                //         </div>
-                //         <div class="age">${item.age}</div>
-                //     `;
+                    card.innerHTML = `
+                        <img src ="${item.photo}" alt="photo">
+                        <div class"name">${item.name} ${item.surname}</div>
+                        <div class="sex">
+                            <img src=${icon} alt="male">
+                        </div>
+                        <div class="age">${item.age}</div>
+                    `;
 
-                //     document.querySelector(".app").appendChild(card);
-                // });
+                    document.querySelector(".app").appendChild(card);
+                });
             } else {
                 console.error("Что-то пошло не так");
             }
